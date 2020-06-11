9fb32a327b9fcc2470b43e5a8afbeda297d89256
X4CY <66755146+X4CY@users.noreply.github.com>
2020-06-11 21:56:37 +0700

    Create net.py

---
+++ net.py
@@ -0,0 +1,148 @@
+#!/bin/bash
+# Author : Hiro-Hiro ID
+clear
+echo "|------------------------------------------|"
+sleep 1
+echo "     TOOLS HACK FB TARGET BY HIRO ID        "
+echo "|------------------------------------------|"
+r="\033[1;31m"
+g="\033[1;32m"
+y="\033[1;33m"
+pur="\033[1;34m"
+p="\033[1;35m"
+c="\033[1;36m"
+w="\033[1;37m"
+hari=`date +%A`
+tanggal=`date +%d`
+bulan=`date +%B`
+tahun=`date +%Y`
+waktu=`date +%T`
+zona=`date +%Z`
+sleep 1
+echo ""
+echo ""
+echo "${g}+------------------------------------------+"
+echo "Selamat Datang Kawan Semoga Bermanfaat"
+echo "+------------------------------------------+"
+sleep 1
+echo ""
+echo "${r}+------------------------------------------+"
+echo "Jangan Salah Gunakan Tools Ini"
+echo "+------------------------------------------+"
+sleep 1
+echo ""
+echo "${y}+------------------------------------------+"
+echo "Sebelumnya Kita Boleh kenalan Dulu "
+sleep 2
+echo -n "Siapakah Nama Kamu ? ";read nm
+sleep 1
+echo "Hallo $nm Senang bisa berkenalan denganmu :) "
+sleep 2
+echo "Eh iya Lupa Perkenalkan Nama Saya Hiro-ID "
+sleep 2
+echo "Sudah Cukup Perkenalannya ya "
+sleep 1
+echo "+------------------------------------------+"
+sleep 1
+echo "${r}Oke Lanjut .... "
+sleep 1
+read -p "Apakah Anda Ingin Menggunakan Tools Ini[y/n]: " yn
+if [ $yn = y ]
+then
+clear
+sleep 1
+echo "${c}|---------------------------------------------|"
+echo "${c}|           Tools Hack Fb Target              |"
+echo "${c}|---------------------------------------------|"
+echo "${c}|(/) Author : Hiro ID                         |"
+echo "${c}|(/) github : https://github.com/hirogans/    |"
+echo "${c}|(/) fanspage : https:/fb.me/hiroganss.id     |"
+echo "${c}|(/) Copyright : ©2020.inc                    |"
+echo "${c}|                                             |"
+echo "${c}|---------------------------------------------|"
+sleep 2
+echo ""
+echo "(/)Sedang memproses tools..."
+sleep 3
+echo ""
+echo "(/)Mengupload Tools..."
+sleep 3
+echo ""
+echo "(/)Mengekstrax Tools..."
+sleep 5
+echo ""
+echo "(/)Membuka Tools..."
+sleep 3
+echo ""
+else
+sleep 1
+echo ""
+echo "${g}Terima kasih Telah Mengunjungi Tools ini"
+sleep 1
+echo ""
+echo "${c}(/) Sedang Proses Keluar ... "
+sleep 3
+exit
+fi
+clear
+sleep 2
+echo "${c}|=============================================|"
+echo "${c}|             H4CK FB                         |"
+echo "${c}==============================================="
+echo "${c}|(/) Author : X4 CYB3R.                       |"
+echo "${c}|(/) github : https://github.com/X4CY/        |"
+echo "${c}|(/) IG : @A6REMIXER.                         |"
+echo "${c}|(/) Copyright : ©2020.inc                    |"
+echo "${c}|                                             |"
+echo "${c}|---------SUBS YT ARL PRODUCTION--------------|"
+echo ""
+read -p "Apakah Anda Ingin Melanjutkan [y/n] " nn
+if [ $nn = y ]
+then
+sleep 2
+echo "Login Dulu Menggunakan Akun Fb Anda "
+echo -n "Masukan Username/id fb : ";read uu
+echo -n "Masuka Password fb kamu: ";read ii
+echo "(/)Memeriksa Username Dan Password..."
+sleep 4
+echo "(/)Proses Masuk Akun Anda..."
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+sleep 4
+echo "(+)Login [suscces]  "
+sleep 1
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+echo "Langkah Terakhir.. "
+termux-brightness 0
+termux-brightness 200
+termux-brightness 0
+termux-brightness 200
+sleep 1
+echo "Cari Id Korban Lalu masukan di Sini... "
+else
+exit
+fi
+echo -n "Masukan Id Korban : ";read id
+echo "(/) Membobol target  $id  untuk mendapatkan password ... "
+sleep 6
+echo "Password Target di Temukan.."
+sleep 2
+echo "
++---------------------+
+id = $id
+pw = Alanwalker3782
++---------------------+"
+termux-torch on
+cd /sdcard
+
+sleep 1
+echo "Done ...."
+sleep 1
+
+sleep 1
+exit
